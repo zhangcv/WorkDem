f5cef7bea55c2fd41ded33f346d4c4377e939acc
zhangcv <zhangcv2017@163.com>
2019-10-08 11:32:41 +0800

    修改Test004, 修正125 359.4 不应该进行浮点数运算 增加了新的if条件

--- src/main/java/com/cml/project/project4/Test004.java
+++ src/main/java/com/cml/project/project4/Test004.java
@@ -14,32 +14,33 @@ public class Test004 {
         String str1 = null, str2 = null;
         int i1, i2;
         float f1, f2;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("输入的第一个字符串");
-        str1 = sc.nextLine();
-        System.out.println("输入的第二个字符串");
-        str2 = sc.nextLine();
-
-        if(tryParseInt(str1)&&tryParseInt(str2)){
-            i1 = Integer.parseInt(str1);
-            i2 = Integer.parseInt(str2);
-            String result2 = strAdd(i1, i2);
-            System.out.println("整数处理：" + result2);
+        boolean isRecord = true;
+        while(isRecord) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("输入的第一个字符串");
+            str1 = sc.nextLine();
+            System.out.println("输入的第二个字符串");
+            str2 = sc.nextLine();
+            if (tryParseInt(str1) && tryParseInt(str2)) {
+                i1 = Integer.parseInt(str1);
+                i2 = Integer.parseInt(str2);
+                String result2 = strAdd(i1, i2);
+                System.out.println("整数处理：" + result2);
+                isRecord=false;
+            } else if (!tryParseInt(str1) && !tryParseInt(str2)&&tryParseFloat(str1) && tryParseFloat(str2)) {
+                f1 = Float.parseFloat(str1);
+                f2 = Float.parseFloat(str2);
+                String result3 = strAdd(f1, f2);
+                System.out.println("浮点数处理：" + result3);
+                isRecord=false;
+            } else if (!tryParseFloat(str1) && !tryParseFloat(str2)) {
+                String result1 = strAdd(str1, str2);
+                System.out.println("字符串拼接：" + result1);
+                isRecord=false;
+            } else {
+                System.out.println("请输入两个字母或者两个整数或者两个浮点数");
+            }
         }
-        else if(tryParseFloat(str1)&&tryParseFloat(str2)){
-            f1 = Float.parseFloat(str1);
-            f2 = Float.parseFloat(str2);
-            String result3 = strAdd(f1, f2);
-            System.out.println("浮点数处理：" + result3);
-        }
-        else if(!tryParseFloat(str1)&&!tryParseFloat(str2)) {
-            String result1 = strAdd(str1, str2);
-            System.out.println("字符串拼接：" + result1);
-        }else {
-            System.out.println("请输入两个字母或者两个数字");
-        }
-
-
     }
     static boolean tryParseFloat(String value){
         try{
